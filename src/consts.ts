88dc13f42300465eb7f56f90824abc03c9b87af3
Dex REAU <dev@oqva.digital>
2023-11-15 21:12:30 +0000

    UP

---
+++ src/consts.ts
@@ -0,0 +1,13 @@
+
+// META
+
+export const SITE_TITLE = "Broken Dreams";
+
+export const SITE_DESCRIPTION = "At Broken Dreams, our mobile cafes are more than just a pit-stop for your daily caffeine fix. They are the crossroads where authenticity meets aspiration. We believe a cup of coffee should stir the soul, not just the senses. Every one of our baristas is an artist, a musician, an actor—a dreamer.";
+
+export const SITE_KEYWORDS = "Dolor"
+    
+
+// SOCIAL SHARING IMAGE
+
+export const OG_IMAGE = '/og/social-image.jpg'
